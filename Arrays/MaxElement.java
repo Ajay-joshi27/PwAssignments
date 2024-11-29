4e0077ae95b1af2b4167ff6562d4d9d49abff0a5
Ajay Joshi <ajayajoshi143@gmail.com>
2024-11-29 06:10:37 +0530

    Create MaxElement.java

---
+++ Arrays/MaxElement.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        // System.out.println("Try programiz.pro");
+        int[] arr={-9,2,-6,-4,-1,-2};
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+           
+            } 
+        }
+        System.out.println(max);
+    }
+}
