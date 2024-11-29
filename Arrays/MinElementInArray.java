eb1f46f518e5e8da42ecbab4fd4b6c217fa9b7c9
Ajay Joshi <ajayajoshi143@gmail.com>
2024-11-29 06:11:06 +0530

    Update MinElementInArray.java

--- Arrays/MinElementInArray.java
+++ Arrays/MinElementInArray.java
@@ -2,7 +2,7 @@ class Main {
     public static void main(String[] args) {
         // System.out.println("Try programiz.pro");
         int[] arr={-9,2,-6,-4,-1,-2};
-        int min=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
         for(int i=0;i<arr.length;i++){
             if(arr[i]<min){
                 min=arr[i];
